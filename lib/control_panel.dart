40159a5d1f7fdaa49c2cb4b7c64b40939ce1b03e
Abhishek Sahu <asahu.m1608@gmail.com>
2022-08-11 18:26:16 +0530

    Update control panel widget
    Remove outer `position` widget

--- lib/control_panel.dart
+++ lib/control_panel.dart
@@ -10,66 +10,61 @@ class ControlPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: 0.0,
-      right: 0.0,
-      bottom: 50.0,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(),
-                ),
-                ControlButton(
-                  icon: Icon(Icons.arrow_left),
-                  onPressed: () {
-                    onTapped(Direction.left);
-                  },
-                ),
-              ],
-            ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(),
+              ),
+              ControlButton(
+                icon: Icon(Icons.arrow_left),
+                onPressed: () {
+                  onTapped(Direction.left);
+                },
+              ),
+            ],
           ),
-          Expanded(
-            child: Column(
-              children: [
-                ControlButton(
-                  icon: Icon(Icons.arrow_drop_up_sharp),
-                  onPressed: () {
-                    onTapped(Direction.up);
-                  },
-                ),
-                SizedBox(
-                  height: 75.0,
-                ),
-                ControlButton(
-                  icon: Icon(Icons.arrow_drop_down_sharp),
-                  onPressed: () {
-                    onTapped(Direction.down);
-                  },
-                ),
-              ],
-            ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              ControlButton(
+                icon: Icon(Icons.arrow_drop_up_sharp),
+                onPressed: () {
+                  onTapped(Direction.up);
+                },
+              ),
+              SizedBox(
+                height: 75.0,
+              ),
+              ControlButton(
+                icon: Icon(Icons.arrow_drop_down_sharp),
+                onPressed: () {
+                  onTapped(Direction.down);
+                },
+              ),
+            ],
           ),
-          Expanded(
-            child: Row(
-              children: [
-                ControlButton(
-                  icon: Icon(Icons.arrow_right),
-                  onPressed: () {
-                    onTapped(Direction.right);
-                  },
-                ),
-                Expanded(
-                  child: Container(),
-                ),
-              ],
-            ),
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              ControlButton(
+                icon: Icon(Icons.arrow_right),
+                onPressed: () {
+                  onTapped(Direction.right);
+                },
+              ),
+              Expanded(
+                child: Container(),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
